33ad7f86b86a7238868da7771ad5e19f72fd41e2
Gabriel Dias Bahniuk <gabrieldiasbnu@hotmail.com>
2018-08-06 01:07:26 -0300

    Refresh working

--- Challenge02/src/pages/issues/components/Issue/index.js
+++ Challenge02/src/pages/issues/components/Issue/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
-const Issue = ({ issue }) => {
-  <View>
-    <Image source={{ uri: issue.user.avatar_url }} />
-    <View className="info">
-      <Text className="title">{issue.title}</Text>
-
-      <Text className="login">{issue.user.login}</Text>
+import { View, Text, Image, Linking, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from './styles';
+import { substr } from '../../../../utils';
+const Issue = ({ issue }) => (
+  <View style={styles.container}>
+    <Image style={styles.avatar} source={{ uri: issue.user.avatar_url }} />
+    <View style={styles.infoContainer}>
+      <Text style={styles.title}>{substr(issue.title)}</Text>
+      <Text style={styles.login}>{issue.user.login}</Text>
     </View>
-  </View>;
-};
+    <TouchableOpacity
+      style={styles.buttonIssue}
+      onPress={() => Linking.openURL(issue.html_url)}
+    >
+      <Icon name="angle-right" size={16} />
+    </TouchableOpacity>
+  </View>
+);
 export default Issue;
--- Challenge02/src/pages/issues/index.js
+++ Challenge02/src/pages/issues/index.js
@@ -1,7 +1,7 @@
 import '../../config/ReactotronConfig';
 import React, { Component } from 'react';
 import api from '../../services/api';
-import { View, Text } from 'react-native';
+import { View, FlatList, ActivityIndicator } from 'react-native';
 import styles from './styles';
 import Issue from './components/Issue';
 
@@ -9,17 +9,12 @@ export default class Issues extends Component {
   state = {
     issues: [],
     issueState: 'all',
+    loading: true,
+    refreshing: false,
   };
 
   static navigationOptions = {
     title: 'Issues',
-  };
-
-  loadIssues = async repository => {
-    const response = await api.get(
-      `/repos/${repository}/issues?state=${this.state.issueState}`,
-    );
-    this.setState({ issues: response.data });
   };
 
   componentDidMount() {
@@ -27,10 +22,36 @@ export default class Issues extends Component {
     this.loadIssues(repository.full_name);
   }
 
+  loadIssues = async repository => {
+    this.setState({ refreshing: true });
+    const response = await api.get(
+      `/repos/${repository}/issues?state=${this.state.issueState}`,
+    );
+    this.setState({ issues: response.data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <Issue issue={item} />;
+
+  renderListIssues = () => (
+    <FlatList
+      data={this.state.issues}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+      onRefresh={this.loadIssues(
+        this.props.navigation.state.params.repository.full_name,
+      )}
+      refreshing={this.state.refreshing}
+    />
+  );
+
   render() {
     return (
-      <View>
-        {this.state.issues.map(issue => <Issue issue={issue} key={issue.id} />)}
+      <View style={styles.container}>
+        {this.state.loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          this.renderListIssues()
+        )}
       </View>
     );
   }
